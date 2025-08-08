2399ff1302c109d94a34c186f8b913ab6ccfd0fd
Gabriel Rebouças Fernandes <gabriel.reboucas@petrobras.com.br>
2025-08-08 20:46:21 -0300

    Adds support for a custom background image
    Introduces a helper function to set a background image for the application.
    
    This allows for greater visual customization by injecting a base64-encoded image as a CSS background.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,32 @@
 import streamlit as st
+import base64
+
+def set_bg_hack(main_bg):
+    '''
+    A function to unpack an image from root folder and set as bg.
+ 
+    Returns
+    -------
+    The background.
+    '''
+    # set bg name
+    main_bg_ext = "png"
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+# You can replace 'background.png' with the actual name of your image file.
+# Make sure the image is in the same folder as your script.
+set_bg_hack('teste.png')
 
 st.title("🎈 My new app")
 st.write(
